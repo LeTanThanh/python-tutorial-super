98ebc3c326b6b98c53192f4d5239193086df2653
thanhlt-1007 <le.tan.thanh@sun-asterisk.com>
2024-06-03 16:07:40 +0700

    Put it all together

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+from employee.base_employee import BaseEmployee
+from employee.sales_employee import SalesEmployee
+
 # Python super
 
 ## Introduction to the Python super
@@ -5,3 +8,9 @@
 ## super().__init__()
 
 ## Delegating to other methods in the parent class
+
+## Put it all together
+
+if __name__ == "__main__":
+  sales_employee = SalesEmployee("John", 5_000, 1_000, 2_000)
+  print(sales_employee.get_pay())
